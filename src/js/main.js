86bf3386a5a8f6e3af463e307f662a82ac137ee8
malin <malin.rosen@medieinstitutet.se>
2023-11-17 13:53:34 +0100

    remove function

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,55 @@
 import "./../scss/style.scss";
+import { Tasks } from "./models/Tasks";
 
-let todoList = ["Tvätta", "Plugga", "Handla mat", "Träna"];
 
+const header = document.createElement("header");
+document.body.appendChild(header);
 
+const headerh1 = document.createElement("h1");
+header.appendChild(headerh1);
+headerh1.innerHTML = "TO DO-LIST"
+
+if (localStorage.getItem("todoList")) {
+    todoList = JSON.parse(localStorage.getItem("todoList"));
+   }
+
+const task1 = new Tasks("Tvätta", true);
+const task2 = new Tasks("Städa", false);
+const task3 = new Tasks("Handla mat", false);
+const task4 = new Tasks("Plugga", false);
+
+const todoList = [task1, task2, task3, task4];
+const createHtml = () => {
 const toDosContainer = document.getElementById("toDoContainer");
+toDosContainer.innerHTML = "";
+
 const taskUl = document.createElement("ul");
 toDosContainer.appendChild(taskUl);    
 taskUl.className = "todo";
 
 
+localStorage.setItem("todoTasks", JSON.stringify(todoList))
 
 for (let i = 0; i < todoList.length; i++) {
     const taskLi = document.createElement("li");
     const taskH2 = document.createElement("h2");
-    const removeButton = document.createElement("button");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.checked = todoList[i].isDone;
 
+    checkbox.addEventListener("click", () => {
+    todoList.splice(i, 1);
+    createHtml();
+})
     taskUl.appendChild(taskLi);
     taskLi.appendChild(taskH2);
-    taskLi.appendChild(removeButton);
     taskLi.appendChild(checkbox);
 
-    taskH2.innerHTML = todoList[i] + " ";
-    removeButton.innerHTML = "Done ✓";
+    taskH2.innerHTML = todoList[i].title;
+    checkbox.innerHTML = todoList[i].isDone;
     taskLi.className = "toDoLi";
 
-    /*Event*/
-
-    // removeButton.addEventListener("click", () => {
-    //     handleToDoList(todoList[i]);
-    //   });
-
-
-    // removeButton.addEventListener("click", () => {
-    //     todoList.splice(i, 1);
-    // //   createHtml();
-    // });
-
-
+}
 }
 
+createHtml();
