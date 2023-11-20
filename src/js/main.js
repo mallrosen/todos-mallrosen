45cd9cf5c25a53ef393992ec3ab94d050bac562b
malin <malin.rosen@medieinstitutet.se>
2023-11-20 12:29:20 +0100

    changed value of boolean if ckecked checkbox + style and an input+button

--- src/js/main.js
+++ src/js/main.js
@@ -13,34 +13,39 @@ if (localStorage.getItem("todoList")) {
     todoList = JSON.parse(localStorage.getItem("todoList"));
    }
 
-const task1 = new Tasks("Tvätta", true);
+const task1 = new Tasks("Tvätta", false);
 const task2 = new Tasks("Städa", false);
 const task3 = new Tasks("Handla mat", false)
 const task4 = new Tasks("Plugga", false);
 
 let todoList = [task1, task2, task3, task4];
+
 const createHtml = () => {
-const toDosContainer = document.getElementById("toDoContainer");
-toDosContainer.innerHTML = "";
+const appContainer = document.getElementById("app");
+appContainer.innerHTML = "";
 
 const taskUl = document.createElement("ul");
-toDosContainer.appendChild(taskUl);    
+appContainer.appendChild(taskUl);    
 taskUl.className = "todo";
 
 
 localStorage.setItem("todoTasks", JSON.stringify(todoList))
 
-for (let i = 0; i < todoList.length; i++) {
+for (let i = 0; i < todoList.length; i++){
+    if (todoList[i].isDone === false){
+
     const taskLi = document.createElement("li");
     const taskH2 = document.createElement("h2");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.checked = todoList[i].isDone;
 
-    checkbox.addEventListener("click", () => {
-    todoList.splice(i, 1);
-    createHtml();
-})
+    checkbox.addEventListener("click", () => {  
+        todoList[i].isDone = true;
+        console.log(todoList[i].isDone);
+        createHtml();
+    })
+
     taskUl.appendChild(taskLi);
     taskLi.appendChild(taskH2);
     taskLi.appendChild(checkbox);
@@ -49,23 +54,36 @@ for (let i = 0; i < todoList.length; i++) {
     checkbox.innerHTML = todoList[i].isDone;
     taskLi.className = "toDoLi";
 }
-
+}
 }
 
 createHtml();
 
 
+const addTaskContainer = document.createElement("div")
+addTaskContainer.className = "input-task";
+const taskBtn = document.createElement("button");
+
+taskBtn.innerHTML = "Lägg till!"
+
+const textBox = document.createElement("input");
+
+textBox.setAttribute("type", "textarea");
+textBox.className = "textBox";
+
+addTaskContainer.appendChild(textBox);
+addTaskContainer.appendChild(taskBtn);
+
+document.body.appendChild(addTaskContainer);
 
 
-// const footer = document.createElement("footer");
-// const fDiv = document.createElement("div");
-// const fA = document.createElement("a");
-// const fP = document.createElement("p");
-// document.body.appendChild(footer);
 
-// footer.appendChild(fDiv);
-// fDiv.appendChild(fA);
-// fA.appendChild(fP);
+const footer = document.createElement("footer");
+const fP = document.createElement("p");
+document.body.appendChild(footer);
 
-// fP.innerHTML = "&copy;FED23S-2023";
+footer.appendChild(fP);
 
+fP.innerHTML = "&copy;FED23S-2023";
+
+ 
